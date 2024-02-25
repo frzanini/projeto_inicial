a5a5336a97c9a393f1e1d62eaf15f68ca78e7ac5
frzanini <frzanini@gmail.com>
2024-02-25 11:30:09 -0400

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,26 @@
+print('Jornada de DAdos')
+
+
+# variaveis
+
+idade = 40
+nome = "Fabio R Zanini"
+
+print('Idade:', idade)
+
+#soma
+
+nova_idade = idade + 1
+
+print('Idade Calc:', nova_idade)
+
+# lista 
+
+lista = [1,2,3,4,5,6]
+print(lista)
+
+# Metodos
+
+lista_invertida = lista.reverse()
+
+# print(lista_invertida)
